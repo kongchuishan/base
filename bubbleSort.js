64632b3597491b359ef915aedd841c05665f2f8f
kongchuishan <chinakcs@163.com>
2015-07-07 14:44:32 +0800

    change bubbleSort.js

--- bubbleSort.js
+++ bubbleSort.js
@@ -6,15 +6,23 @@
     function bubbleSort(arr){
         var n=arr.length;//获取数组的长度，既有n个数在排序；
         var tmp=null;//定义一个临时变量，作交换数据用；
+        var bFlag=false;//判断是否排序
         for(var i=0;i<n-1;i++){ //外层循环n-1次
             for(var j=0;j<n-1-i;j++){//内层循环n-1-i次，每次循环都将最大的值放到n-1-i的位置上
                 if(arr[j]>arr[j+1]){
                     tmp=arr[j];
                     arr[j]=arr[j+1];
                     arr[j+1]=tmp;
+                    bFlag=true;
                 }
             }
+            if(bFlag){
+                bFlag=false;
+            }else{
+                break;//走到这里说明不用排序，优化性能直接跳出
+            }
         }
+
         return arr;
     }
     console.log(bubbleSort(arr));
